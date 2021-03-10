e365272f0ddbe2b02bd28301e5b234e02e5679da
Pieter van den Ham <p.e.vandenham@gmail.com>
2021-03-10 09:11:58 +0100

    Use BM25 to generate examples

--- libsvmgenerator/src/main/java/nl/tudelft/ir/LibSvmFileGenerator.java
+++ libsvmgenerator/src/main/java/nl/tudelft/ir/LibSvmFileGenerator.java
@@ -1,7 +1,11 @@
 package nl.tudelft.ir;
 
 import io.anserini.analysis.AnalyzerUtils;
+import io.anserini.analysis.DefaultEnglishAnalyzer;
+import io.anserini.index.IndexArgs;
 import io.anserini.index.IndexReaderUtils;
+import io.anserini.search.query.BagOfWordsQueryGenerator;
+import io.anserini.search.query.QueryGenerator;
 import nl.tudelft.ir.dataset.Datasets;
 import nl.tudelft.ir.dataset.QRel;
 import nl.tudelft.ir.feature.Feature;
@@ -11,22 +15,44 @@ import nl.tudelft.ir.index.DocumentCollection;
 import nl.tudelft.ir.index.Index;
 import nl.tudelft.ir.index.LuceneIndex;
 import nl.tudelft.ir.util.ProgressIndicator;
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BM25Similarity;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LibSvmFileGenerator {
+
+    private final IndexReader indexReader;
+
+    private final Index index;
+
+    public LibSvmFileGenerator(Index index, IndexReader indexReader, Path queriesPath, Path top100Path, Path qrelsPath, List<Feature> features) {
+        this.index = index;
+        this.indexReader = indexReader;
+        this.queriesPath = queriesPath;
+        this.top100Path = top100Path;
+        this.qrelsPath = qrelsPath;
+        this.features = features;
+    }
+
+    private final Path queriesPath;
+    private final Path top100Path;
+    private final Path qrelsPath;
+    private final List<Feature> features;
 
     public static void main(String[] args) throws IOException {
         IndexReader indexReader = IndexReaderUtils.getReader(args[0]);
@@ -34,7 +60,7 @@ public class LibSvmFileGenerator {
         String modus = "dev";
         String size = "";
         long maxNumTriples = 1_000_000;
-        int negativeDocsPerPositiveDoc = 100;
+        int negativeDocsPerPositiveDoc = 2;
 
         String dataDirectory = "data";
 
@@ -47,22 +73,16 @@ public class LibSvmFileGenerator {
 
         List<Feature> features = Features.LAMBDAMART_DEFAULT_FEATURES;
 
-        LibSvmFileGenerator generator = new LibSvmFileGenerator(index, queriesPath, top100Path, qrelsPath, features);
+        LibSvmFileGenerator generator = new LibSvmFileGenerator(index, indexReader, queriesPath, top100Path, qrelsPath, features);
         generator.generateFromTop100(maxNumTriples, outputPath, negativeDocsPerPositiveDoc);
     }
 
-    private final Index index;
-    private final Path queriesPath;
-    private final Path top100Path;
-    private final Path qrelsPath;
-    private final List<Feature> features;
-
-    public LibSvmFileGenerator(Index index, Path queriesPath, Path top100Path, Path qrelsPath, List<Feature> features) {
-        this.index = index;
-        this.queriesPath = queriesPath;
-        this.top100Path = top100Path;
-        this.qrelsPath = qrelsPath;
-        this.features = features;
+    private static TopDocs search(IndexSearcher searcher, Query query, int n) {
+        try {
+            return searcher.search(query, n);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void generateFromTop100(long maxNumTriples, Path outputPath, int negativeDocsPerPositiveDoc) {
@@ -71,19 +91,11 @@ public class LibSvmFileGenerator {
         System.out.println("Loading " + queriesPath + "...");
         Map<String, String> queries = Datasets.loadQueries(queriesPath);
 
-        // This query does not seem to have a top100 entry
-        queries.remove("502557");
-
-        System.out.println("Loading " + top100Path + "...");
-        Map<String, List<String>> top100 = Datasets.loadTop100(top100Path);
-
         System.out.println("Loading " + qrelsPath + "...");
         Map<String, QRel> qrels = Datasets.loadQrels(qrelsPath);
 
-        removePositiveExamplesFromTop100(qrels, top100);
-
         System.out.println("Generating triples...");
-        List<Example> examples = generateTriples(queries, top100, qrels, negativeDocsPerPositiveDoc)
+        List<Example> examples = generateTriples(queries, qrels, negativeDocsPerPositiveDoc)
                 .limit(maxNumTriples)
                 .collect(Collectors.toList());
 
@@ -120,52 +132,6 @@ public class LibSvmFileGenerator {
         System.out.println("Done in " + String.format("%.1f", (float) duration / 1000) + "s");
     }
 
-    private void removePositiveExamplesFromTop100(Map<String, QRel> qrels, Map<String, List<String>> top100) {
-        for (String queryId : top100.keySet()) {
-            String positiveDocId = qrels.get(queryId).getDocId();
-            top100.get(queryId).remove(positiveDocId);
-        }
-    }
-
-    private Stream<Example> generateTriples(Map<String, String> queries, Map<String, List<String>> top100, Map<String, QRel> qrels, int negativeDocsPerPositiveDoc) {
-        List<String> allDocumentIds = top100.values().stream().flatMap(Collection::stream).distinct().collect(Collectors.toList());
-
-        return queries.entrySet().stream()
-                .flatMap(entry -> {
-                    String queryId = entry.getKey();
-                    String query = entry.getValue();
-
-                    // Grab random non-relevant documents from top-100
-                    // We define a non-relevant document to be a document that occurs
-                    // in the top-100 of a *different* query
-                    ThreadLocalRandom random = ThreadLocalRandom.current();
-                    List<String> negativeDocIds = random.ints(0, allDocumentIds.size())
-                            .limit(negativeDocsPerPositiveDoc)
-                            .mapToObj(allDocumentIds::get)
-                            .collect(Collectors.toList());
-
-                    QRel qrel = qrels.get(queryId);
-                    if (!qrel.getLabel().equals("1")) {
-                        throw new RuntimeException("QRel unexpectedly has label " + qrel.getLabel() + " (expected \"1\")");
-                    }
-
-                    String positiveDocId = qrel.getDocId();
-
-                    List<Example> examples = new ArrayList<>();
-                    for (String negativeDocId : negativeDocIds) {
-                        if (negativeDocId.equals(positiveDocId)) {
-                            // theoretically possible, but a tiny chance
-                            continue;
-                        }
-
-                        examples.add(Example.positive(queryId, query, positiveDocId));
-                        examples.add(Example.negative(queryId, query, negativeDocId));
-                    }
-
-                    return examples.stream();
-                });
-    }
-
     private LibSvmEntry generateLibSvmEntry(Example example, DocumentCollection documentCollection) {
         List<String> queryTerms = AnalyzerUtils.analyze(example.query);
         Document document = documentCollection.find(example.docId);
@@ -173,6 +139,48 @@ public class LibSvmFileGenerator {
         float[] featuresVec = Features.generateVector(features, queryTerms, document, documentCollection);
 
         return new LibSvmEntry(example.label, example.queryId, featuresVec, example.docId);
+    }
+
+    private Stream<Example> generateTriples(Map<String, String> queries, Map<String, QRel> qrels, int negativeDocsPerPositiveDoc) {
+        IndexSearcher searcher = new IndexSearcher(indexReader);
+        searcher.setSimilarity(new BM25Similarity());
+
+        return qrels.entrySet().stream()
+                .limit(500_000)
+                .parallel()
+                .flatMap(entry -> {
+                    QRel qrel = entry.getValue();
+                    String queryId = qrel.getQid();
+                    String queryString = queries.get(queryId);
+
+                    if (queryString == null) {
+                        throw new RuntimeException("Query " + queryId + " does not exist");
+                    }
+
+                    Analyzer analyzer = DefaultEnglishAnalyzer.newDefaultInstance();
+                    QueryGenerator queryGenerator = new BagOfWordsQueryGenerator();
+                    Query query = queryGenerator.buildQuery(IndexArgs.CONTENTS, analyzer, queryString);
+
+                    TopDocs rs = search(searcher, query, negativeDocsPerPositiveDoc);
+
+                    List<String> bm25DocumentIds = Arrays.stream(rs.scoreDocs)
+                            .map(scoreDoc -> IndexReaderUtils.convertLuceneDocidToDocid(indexReader, scoreDoc.doc))
+                            .distinct()
+                            .collect(Collectors.toList());
+
+                    String positiveDocId = qrel.getDocId();
+
+                    bm25DocumentIds.remove(positiveDocId);
+
+                    List<Example> examples = new ArrayList<>();
+                    examples.add(Example.positive(queryId, queryString, positiveDocId));
+
+                    for (String negativeDocId : bm25DocumentIds) {
+                        examples.add(Example.negative(queryId, queryString, negativeDocId));
+                    }
+
+                    return examples.stream();
+                });
     }
 
     private static class Example {
